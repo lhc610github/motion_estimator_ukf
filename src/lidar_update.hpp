12f07d13a36e06411bbce7e0253085c79a8b2daa
lhc610github <604340918@qq.com>
2019-04-04 15:43:56 +0800

    V1.1

---
+++ src/lidar_update.hpp
@@ -0,0 +1,11 @@
+#ifndef LIDAR_UPDATE_HPP_
+#define LIDAR_UPDATE_HPP_
+
+#include "Types.hpp"
+#include "imu_predict.hpp"
+#include <iostream>
+
+template<typename T>
+class Lidar_measurement : public Kalman
+
+#endif
