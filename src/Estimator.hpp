8d70e503202a71b701e44e49c5636da852e5c83a
lhc610github <604340918@qq.com>
2019-10-29 18:35:41 +0800

    test1

--- src/Estimator.hpp
+++ src/Estimator.hpp
@@ -19,9 +19,9 @@ class Filter {
             ImuBuf_Size = 500;
             has_regist_publisher = false;
             // Delay_Size = 80;
-            int Delay_output_period = 12;
+            int Delay_output_period = 13;
             PredictBuf_Size = 60;
-            OutputPeriodCount = 7;
+            OutputPeriodCount = 4;
             Delay_Size = Delay_output_period * OutputPeriodCount;
             PredictIndex = 0;
             outputpredictor_filter_ptr = new OutputPredictor<T>(Delay_output_period, OutputPeriodCount);
@@ -141,7 +141,7 @@ class Filter {
                     restart_filter();
                     filter_mutex.unlock();
                     return;
-                } else if (_dt > 1.0f) {
+                } else if (_dt > 1.7f) {
                     std::cout << "[filter]: predict dt to large ? : "<< _dt << std::endl;
                     // std::cout << "[filter]: imu_t: "<< ImuBuf[_i].t << " vio_t: " << PredictBuf.rbegin()->t<< std::endl;
                     printf("[filter]: imu_t: %.4f,  vio_t: %.4f\n", ImuBuf[_i].t, PredictBuf.rbegin()->t);
--- src/estimator_publisher.hpp
+++ src/estimator_publisher.hpp
@@ -21,42 +21,42 @@ class EstimatorPublisher {
         }
         
         void PublisherRegist(ros::NodeHandle& _nh) {
-            pos_pub = _nh.advertise<geometry_msgs::PoseStamped>("/vio_data_rigid1/pos", 100);
-            vel_pub = _nh.advertise<geometry_msgs::Vector3Stamped>("/vio_data_rigid1/vel", 100);
+//            pos_pub = _nh.advertise<geometry_msgs::PoseStamped>("/vio_data_rigid1/pos", 100);
+//            vel_pub = _nh.advertise<geometry_msgs::Vector3Stamped>("/vio_data_rigid1/vel", 100);
             acc_pub = _nh.advertise<geometry_msgs::Vector3Stamped>("/vio_data_rigid1/acc", 100);
-            att_pub = _nh.advertise<geometry_msgs::PoseStamped>("/vio_data_rigid1/att", 100);
+//            att_pub = _nh.advertise<geometry_msgs::PoseStamped>("/vio_data_rigid1/att", 100);
             ukf_pub = _nh.advertise<nav_msgs::Odometry>("/ukf/odometry", 100);
             ukf_predict_pub = _nh.advertise<nav_msgs::Odometry>("/ukf_predict/odometry", 100);
-            lidar_pub = _nh.advertise<sensor_msgs::FluidPressure>("/lidar_data", 100);
+//            lidar_pub = _nh.advertise<sensor_msgs::FluidPressure>("/lidar_data", 100);
             has_register = true;
         }
 
         void PosPub(const Eigen::Vector3d& _pos, const Eigen::Quaterniond& _q_att, double _t) {
             if (!has_register)
                 return;
-            geometry_msgs::PoseStamped _tmp_msg;
-            _tmp_msg.header.frame_id = "world";
-            _tmp_msg.header.stamp = ros::Time(_t);
-            _tmp_msg.pose.position.x = _pos(0);
-            _tmp_msg.pose.position.y = -_pos(1);
-            _tmp_msg.pose.position.z = -_pos(2);
-            _tmp_msg.pose.orientation.w = _q_att.w();
-            _tmp_msg.pose.orientation.x = _q_att.x();
-            _tmp_msg.pose.orientation.y = -_q_att.y();
-            _tmp_msg.pose.orientation.z = -_q_att.z();
-            pos_pub.publish(_tmp_msg);
+//            geometry_msgs::PoseStamped _tmp_msg;
+//            _tmp_msg.header.frame_id = "world";
+//            _tmp_msg.header.stamp = ros::Time(_t);
+//            _tmp_msg.pose.position.x = _pos(0);
+//            _tmp_msg.pose.position.y = -_pos(1);
+//            _tmp_msg.pose.position.z = -_pos(2);
+//            _tmp_msg.pose.orientation.w = _q_att.w();
+//            _tmp_msg.pose.orientation.x = _q_att.x();
+//            _tmp_msg.pose.orientation.y = -_q_att.y();
+//            _tmp_msg.pose.orientation.z = -_q_att.z();
+//            pos_pub.publish(_tmp_msg);
         }
-
+//
         void VelPub(const Eigen::Vector3d& _vel, double _t) {
             if (!has_register)
                 return;
-            geometry_msgs::Vector3Stamped _tmp_msg;
-            _tmp_msg.header.frame_id = "world";
-            _tmp_msg.header.stamp = ros::Time(_t);
-            _tmp_msg.vector.x = _vel(0);
-            _tmp_msg.vector.y = -_vel(1);
-            _tmp_msg.vector.z = -_vel(2);
-            vel_pub.publish(_tmp_msg);
+//            geometry_msgs::Vector3Stamped _tmp_msg;
+//            _tmp_msg.header.frame_id = "world";
+//            _tmp_msg.header.stamp = ros::Time(_t);
+//            _tmp_msg.vector.x = _vel(0);
+//            _tmp_msg.vector.y = -_vel(1);
+//            _tmp_msg.vector.z = -_vel(2);
+//            vel_pub.publish(_tmp_msg);
         }
 
         void AccPub(const Eigen::Vector3d& _acc, double _t) {
@@ -66,33 +66,33 @@ class EstimatorPublisher {
             _tmp_msg.header.frame_id = "world";
             _tmp_msg.header.stamp = ros::Time(_t);
             _tmp_msg.vector.x = _acc(0);
-            _tmp_msg.vector.y = -_acc(1);
-            _tmp_msg.vector.z = -_acc(2);
+            _tmp_msg.vector.y = _acc(1);
+            _tmp_msg.vector.z = _acc(2);
             acc_pub.publish(_tmp_msg);
         }
 
         void AttPub(const Eigen::Quaterniond& _q_att, double _t) {
             if (!has_register)
                 return;
-            geometry_msgs::PoseStamped _tmp_msg;
-            _tmp_msg.header.frame_id = "world";
-            _tmp_msg.header.stamp = ros::Time(_t);
-            _tmp_msg.pose.orientation.w = _q_att.w();
-            _tmp_msg.pose.orientation.x = _q_att.x();
-            _tmp_msg.pose.orientation.y = -_q_att.y();
-            _tmp_msg.pose.orientation.z = -_q_att.z();
-            att_pub.publish(_tmp_msg);
+//            geometry_msgs::PoseStamped _tmp_msg;
+//            _tmp_msg.header.frame_id = "world";
+//            _tmp_msg.header.stamp = ros::Time(_t);
+//            _tmp_msg.pose.orientation.w = _q_att.w();
+//            _tmp_msg.pose.orientation.x = _q_att.x();
+//            _tmp_msg.pose.orientation.y = -_q_att.y();
+//            _tmp_msg.pose.orientation.z = -_q_att.z();
+//            att_pub.publish(_tmp_msg);
         }
-
+//
         void LidarPub(const double& _lidar_data, const double& _distant, const double& t) {
             if (!has_register)
                 return;
-            sensor_msgs::FluidPressure _tmp_msg;
-            _tmp_msg.header.frame_id = "world";
-            _tmp_msg.header.stamp = ros::Time(t);
-            _tmp_msg.fluid_pressure = _distant;
-            _tmp_msg.variance = _lidar_data;
-            lidar_pub.publish(_tmp_msg);
+//            sensor_msgs::FluidPressure _tmp_msg;
+//            _tmp_msg.header.frame_id = "world";
+//            _tmp_msg.header.stamp = ros::Time(t);
+//            _tmp_msg.fluid_pressure = _distant;
+//            _tmp_msg.variance = _lidar_data;
+//            lidar_pub.publish(_tmp_msg);
         }
 
         void UkfPub(const Eigen::Vector3d& _pos, const Eigen::Vector3d& _vel, const Eigen::Vector3d& _acc,
@@ -179,11 +179,11 @@ class EstimatorPublisher {
     private:
         ros::Publisher ukf_pub;
         ros::Publisher ukf_predict_pub;
-        ros::Publisher lidar_pub;
-        ros::Publisher pos_pub;
-        ros::Publisher vel_pub;
+//        ros::Publisher lidar_pub;
+//        ros::Publisher pos_pub;
+//        ros::Publisher vel_pub;
         ros::Publisher acc_pub;
-        ros::Publisher att_pub;
+//        ros::Publisher att_pub;
         bool has_register;
 };
 
